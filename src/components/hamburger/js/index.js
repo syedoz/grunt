0a17d657e6d41d95b9336471d00556e21fc88a70
Syed Moiz <moiz.ali@tigerspike.com>
2019-02-12 05:13:46 +0400

    updated structure

---
+++ src/components/hamburger/js/index.js
@@ -0,0 +1,4 @@
+$('#nav-icon3').click(function(){
+    $(this).toggleClass('open');
+    $('#toggling_nav').toggleClass('nav_open');
+});
---
+++ src/components/slider/js/index.js
@@ -0,0 +1,25 @@
+// Highlights slider
+$('#highlights').owlCarousel({
+    center: true,
+    items:5,
+    loop:true,
+    margin:10,
+    responsive:{
+        600:{
+            items:4
+        }
+    }
+})
+
+// Posts slider
+$('#posts').owlCarousel({
+    center: true,
+    items:5,
+    loop:true,
+    margin:10,
+    responsive:{
+        600:{
+            items:4
+        }
+    }
+})
